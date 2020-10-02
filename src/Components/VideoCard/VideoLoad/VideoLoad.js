25401795f6b2de2e8e438ade19948a5369745982
Natesh <natesh246@gmail.com>
2020-10-02 10:35:43 +0530

    added video load in home

---
+++ src/Components/VideoCard/VideoLoad/VideoLoad.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./VideoLoad.css";
+
+function VideoLoad() {
+  return (
+    <div className="video-load">
+      <div className="video-load__image"></div>
+      <div className="video-load__desc">
+        <div className="video-load__desc-img"></div>
+        <div className="video-load__desc-right">
+          <div className="h2"></div>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoLoad;
